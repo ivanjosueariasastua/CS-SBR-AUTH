db5cb8a246c135d65cdff2d16b68d5e441fae7f4
Iván Josué Arias Astúa <53876880+ivanjosueariasastua@users.noreply.github.com>
2025-05-17 16:29:22 -0600

    Initial commit

---
+++ auth/src/main/java/com/colibri/solvers/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package com.colibri.solvers.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
